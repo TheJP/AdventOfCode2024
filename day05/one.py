f141a83f029e781b96d53f32b32db29c3eda5e12
TheJP <jp_55555@hotmail.com>
2024-12-05 06:28:15 +0100

    Solved day 5 part 1

---
+++ day05/one.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+import sys
+
+rules = defaultdict(lambda: [])
+
+input = open(sys.argv[1]).read().splitlines()
+rules_part = True
+total = 0
+for line in input:
+    if line.strip() == "":
+        rules_part = False
+        continue
+
+    if rules_part:
+        x, y = line.split("|")
+        rules[x].append(y)
+    else:
+        order = line.split(",")
+        correct = True
+        for i in range(len(order)):
+            for j in range(i+1, len(order)):
+                if order[i] in rules[order[j]]:
+                    correct = False
+
+        if correct:
+            total += int(order[len(order) // 2])
+
+print(total)
