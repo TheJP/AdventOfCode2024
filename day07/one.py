e0b1ecad37a511a08fd436221302f3c237eba35d
TheJP <jp_55555@hotmail.com>
2024-12-07 06:23:18 +0100

    Solved day 7 part 1

---
+++ day07/one.py
@@ -0,0 +1,28 @@
+import sys
+
+total = 0
+
+def solve(r, ops):
+    if len(ops) == 0:
+        yield 0
+        return
+
+    rhs = ops[0]
+    lhss = solve(r, ops[1:])
+    for lhs in lhss:
+        if lhs > r:
+            continue
+        yield lhs + rhs
+        yield lhs * rhs
+
+input = open(sys.argv[1]).read().splitlines()
+for line in input:
+    result, operands = line.split(":")
+    result = int(result)
+    operands = list(map(int, operands.split()))
+
+    if result in solve(result, list(reversed(operands))):
+        # print("YES", line)
+        total += result
+
+print(total)
