e53f310d14ab2b8aace60d16e30109aa569b6e51
TheJP <jp_55555@hotmail.com>
2024-12-09 14:21:12 +0100

    Day 9 part 2: >100x faster solution

--- day09/two.py
+++ day09/two.py
@@ -1,20 +1,45 @@
+# >100x faster with some simple changes.
+# There is still a lot of potential to further improve,
+# but I'm content with a runtime that is less than 150ms.
 import sys
 
 disk = []
+gaps = []
+
+def show():
+    global disk
+    for d in disk:
+        if d >= 0:
+            print(d, end="")
+        else:
+            print(".", end="")
+    print()
 
 id = 0
 input = open(sys.argv[1]).read().splitlines()
 for line in input:
     free = False
     for c in line:
+        c = int(c)
         if not free:
-            for i in range(int(c)):
+            for i in range(c):
                 disk.append(id)
             id += 1
         else:
-            for i in range(int(c)):
+            if c > 0:
+                gaps.append((len(disk), c))
+            for i in range(c):
                 disk.append(-1)
         free = not free
+
+best = [None] * 10
+for i in range(1, 10):
+    for (gi, (_di, size)) in enumerate(gaps):
+        if size >= i:
+            best[i] = gi
+            break
+# print(gaps)
+# print(best)
 
 done_id = set()
 r = len(disk) - 1
@@ -23,7 +48,6 @@ while r >= 1:
         r -= 1
     if r < 1:
         break
-    # print(r)
 
     file_size = 0
     id = disk[r]
@@ -33,26 +57,32 @@ while r >= 1:
         file_size += 1
     done_id.add(id)
 
-    hole = 0
-    while hole < r:
-        space = 0
-        while hole < r and disk[hole] >= 0:
-            hole += 1
-        for i in range(hole, r):
-            if disk[i] >= 0:
-                break
-            space += 1
+    if best[file_size] is None:
+        r -= file_size
+        continue
 
-        if file_size > space:
-            hole += space
-            continue
-        if hole >= r:
+    for gi in range(best[file_size], len(gaps)):
+        di, space = gaps[gi]
+        if space >= file_size:
+            best[file_size] = gi
             break
 
-        for i in range(file_size):
-            disk[hole + i], disk[r - i] = disk[r - i], disk[hole + i]
-        break
+    gi = best[file_size]
+    di, space = gaps[gi]
+    if space < file_size:
+        best[file_size] = None
+        continue
+
+    if di >= r:
+        continue
+
+    for i in range(file_size):
+        disk[di + i], disk[r - i] = disk[r - i], disk[di + i]
+    gaps[gi] = (di + file_size, space - file_size)
+    # show()
 
     r -= file_size
 
+
+# show()
 print(sum(pos * id for pos, id in enumerate(disk) if disk[pos] >= 0))
---
+++ day09/two_slow.py
@@ -0,0 +1,58 @@
+import sys
+
+disk = []
+
+id = 0
+input = open(sys.argv[1]).read().splitlines()
+for line in input:
+    free = False
+    for c in line:
+        if not free:
+            for i in range(int(c)):
+                disk.append(id)
+            id += 1
+        else:
+            for i in range(int(c)):
+                disk.append(-1)
+        free = not free
+
+done_id = set()
+r = len(disk) - 1
+while r >= 1:
+    while r >= 0 and (disk[r] < 0 or disk[r] in done_id):
+        r -= 1
+    if r < 1:
+        break
+    # print(r)
+
+    file_size = 0
+    id = disk[r]
+    for i in range(r, 0, -1):
+        if disk[i] != id:
+            break
+        file_size += 1
+    done_id.add(id)
+
+    hole = 0
+    while hole < r:
+        space = 0
+        while hole < r and disk[hole] >= 0:
+            hole += 1
+        for i in range(hole, r):
+            if disk[i] >= 0:
+                break
+            space += 1
+
+        if file_size > space:
+            hole += space
+            continue
+        if hole >= r:
+            break
+
+        for i in range(file_size):
+            disk[hole + i], disk[r - i] = disk[r - i], disk[hole + i]
+        break
+
+    r -= file_size
+
+print(sum(pos * id for pos, id in enumerate(disk) if disk[pos] >= 0))
