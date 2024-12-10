98294a5534f6f9121e6a2af1a2dd228cd7261e5a
TheJP <jp_55555@hotmail.com>
2024-12-10 06:31:32 +0100

    Solved day 10 part 2

---
+++ day10/two.py
@@ -0,0 +1,33 @@
+import sys
+
+input = open(sys.argv[1]).read().splitlines()
+map = [["#"] + [int(e) for e in line] + ["#"] for line in input]
+border = [["#"] * len(map[0])]
+map = border + map + border
+
+def search(x, y):
+    global map
+    visited = set()
+    stack = [(x, y)]
+    sum = 0
+    while len(stack) != 0:
+        (x, y) = stack.pop()
+        if (x, y) in visited:
+            continue
+        # visited.add((x, y))
+        if map[y][x] == 9:
+            sum += 1
+            continue
+        for (dx, dy) in [1, 0], [0, -1], [-1, 0], [0, 1]:
+            if map[y + dy][x + dx] == map[y][x] + 1:
+                stack.append((x + dx, y + dy))
+
+    return sum
+
+total = 0
+for y in range(1, len(map) - 1):
+    for x in range(1, len(map[y]) - 1):
+        if map[y][x] == 0:
+            total += search(x, y)
+
+print(total)
